c70139ea2bf3a02fdd861996ccf9eeda442abd51
A Sama <channelasama@gmail.com>
2019-11-08 14:21:16 +0700

    add object

--- TikiScrapy/TikiScrapy/pipelines.py
+++ TikiScrapy/TikiScrapy/pipelines.py
@@ -18,5 +18,27 @@ class TikiscrapyPipeline(object):
         self.collection = db["quotes_tb"]
 
     def process_item(self, item, spider):
-        #self.collection.insert(dict(item))
+        list_title = item["title"]
+        list_product_id = item["product_id"]
+        list_data_id = item["data_id"]
+        list_image = item["image"]
+        list_final_price = item["final_price"]
+        list_price_regular = item["price_regular"]
+        list_discount = item["discount"]
+        list_link = item["link"]
+        results = list()
+        for i in range(len(list_title)):
+            sp_dict = dict()
+            sp_dict["title"] = list_title[i]
+            sp_dict["product_id"] = list_product_id[i]
+            sp_dict["data_id"] = list_data_id[i]
+            sp_dict["image"] = list_image[i]
+            sp_dict["final_price"] = list_final_price[i]
+            sp_dict["price_regular"] = list_price_regular[i]
+            sp_dict["discount"] = list_discount[i]
+            sp_dict["link"] = list_link[i]
+
+            results.append(sp_dict)
+
+        self.collection.insert(results)
         return item
--- TikiScrapy/TikiScrapy/spiders/tiki_spider.py
+++ TikiScrapy/TikiScrapy/spiders/tiki_spider.py
@@ -1,29 +1,42 @@
 import scrapy
 from ..items import TikiscrapyItem
+
 class TikiSpider(scrapy.Spider) :
     name = "tiki"
     page_number = 2
     start_urls = [
-        'https://tiki.vn/dien-thoai-may-tinh-bang/c1789/xiaomi?src=c.1789.hamburger_menu_fly_out_banner',
+        #'https://tiki.vn/dien-thoai-may-tinh-bang/c1789/xiaomi?src=c.1789.hamburger_menu_fly_out_banner',
+        'https://tiki.vn/dien-thoai-may-tinh-bang/c1789/apple?src=mega-menu'
     ]
 
     def parse(self, response):
 
         items = TikiscrapyItem()
-       # title = response.xpath("//div[@class='content ']/p[@class='title']").extract()
+
         allsp = response.xpath("//div[@class='product-box-list']")
         for sp in allsp :
-            items["title"] = sp.xpath("//div/@data-title").extract()
-            items["product_id"] = sp.xpath("//div/@data-seller-product-id").extract()
-            items["data_id"] = sp.xpath("//div/@data-id").extract()
-            items["image"] = sp.xpath("//div[@class='content ']//span[@class='image']//img/@src").extract()
-            items["final_price"] = sp.xpath("//div/@data-price").extract()
-            items["price_regular"] = sp.xpath("//div[@class='content ']//p[@class='price-sale']//span[@class='price-regular']/text()").extract()
-            items["discount"] = sp.xpath("//div[@class='content ']//p[@class='price-sale']//span[@class='sale-tag sale-tag-square']/text()").extract()
-            items["link"] = sp.xpath("//div[@class='product-item    ']//a/@href").extract()
+            title = sp.xpath("//div/@data-title").extract()
+            product_id = sp.xpath("//div/@data-seller-product-id").extract()
+            data_id = sp.xpath("//div/@data-id").extract()
+            image = sp.xpath("//div[@class='content ']//span[@class='image']//img/@src").extract()
+            final_price = sp.xpath("//div/@data-price").extract()
+            price_regular = sp.xpath("//div[@class='content ']//p[@class='price-sale']//span[@class='price-regular']/text()").extract()
+            discount = sp.xpath("//div[@class='content ']//p[@class='price-sale']//span[@class='sale-tag sale-tag-square']/text()").extract()
+            link = sp.xpath("//div[@class='product-item    ']//a/@href").extract()
+
+            items["title"] = title
+            items["product_id"] = product_id
+            items["data_id"] = data_id
+            items["image"] = image
+            items["final_price"] = final_price
+            items["price_regular"] = price_regular
+            items["discount"] = discount
+            items["link"] = link
+
             yield items
 
-        next_page = "https://tiki.vn/dien-thoai-may-tinh-bang/c1789/xiaomi?src=c.1789.hamburger_menu_fly_out_banner&page=" + str(TikiSpider.page_number)
+        #next_page = "https://tiki.vn/dien-thoai-may-tinh-bang/c1789/xiaomi?src=c.1789.hamburger_menu_fly_out_banner&page=" + str(TikiSpider.page_number)
+        next_page = "https://tiki.vn/dien-thoai-may-tinh-bang/c1789/apple?src=mega-menu&page="+ str(TikiSpider.page_number)
 
         if TikiSpider.page_number <= 3:
             TikiSpider.page_number += 1
